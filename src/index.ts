bb07eba1934f7e8b2aed8a34f81e3098269019ea
yotam <yotam201410@gmail.com>
2024-08-11 15:32:22 +0300

    added error middleware

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import express, { json } from "express";
 import loggerMiddleware from "./middlewares/loggerMiddleware";
+import errorMiddleware from "./middlewares/errorMiddleware";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(loggerMiddleware);
 app.use(json());
-
+app.use(errorMiddleware);
 app.listen(port, () => console.log("server is running on port " + port));
--- src/middlewares/errorMiddleware.ts
+++ src/middlewares/errorMiddleware.ts
@@ -1,11 +1,39 @@
 import { NextFunction, Request, Response } from "express";
 import logger from "../utils/logger";
+import BaseError from "../errors/baseError";
+import { StatusCodes } from "http-status-codes";
 
 const errorMiddleware = async (
+    error: unknown,
     request: Request,
     response: Response,
-    next: NextFunction,
-    error: unknown
-) => {};
+    next: NextFunction
+) => {
+    if (error instanceof BaseError) {
+        logger.warn(
+            `${request.method} ${request.url} resulted in ${error.statusCode} with message ${error.message}`
+        );
+
+        await response.status(error.statusCode).send(error.message);
+    } else if (error instanceof Error) {
+        logger.error(
+            `${request.method} ${request.url} resulted in ${StatusCodes.INTERNAL_SERVER_ERROR} with message ${error.message}`
+        );
+        console.log(error.stack);
+
+        await response
+            .status(StatusCodes.INTERNAL_SERVER_ERROR)
+            .send(error.message);
+    } else {
+        logger.error(
+            `${request.method} ${request.url} resulted in ${StatusCodes.INTERNAL_SERVER_ERROR} with message`
+        );
+
+        await response
+            .status(StatusCodes.INTERNAL_SERVER_ERROR)
+            .send("UNKNOWN");
+    }
+    next(error);
+};
 
 export default errorMiddleware;
