c4e4981ca3c40fe20c7db0f0fc2328f65b114d9e
yotam <yotam201410@gmail.com>
2024-08-11 14:56:40 +0300

    logger middleware

---
+++ src/utils/logger.ts
@@ -0,0 +1,12 @@
+import winston from "winston";
+
+const logger = winston.createLogger({
+  level: process.env.LOGGER_LEVEL || "info",
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: "./logs/logs.log" }),
+  ],
+});
+
+export default logger
