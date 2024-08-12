24508455976bfac80200adf73d8663377d923fad
yotam <yotam201410@gmail.com>
2024-08-12 16:51:22 +0300

    formatting file

--- src/middlewares/errorMiddleware.ts
+++ src/middlewares/errorMiddleware.ts
@@ -4,36 +4,34 @@ import BaseError from "../errors/BaseError";
 import { StatusCodes } from "http-status-codes";
 
 const errorMiddleware = async (
-    error: unknown,
-    request: Request,
-    response: Response,
-    next: NextFunction
+  error: unknown,
+  request: Request,
+  response: Response,
+  next: NextFunction
 ) => {
-    if (error instanceof BaseError) {
-        logger.warn(
-            `${request.method} ${request.url} resulted in ${error.statusCode} with message ${error.message}`
-        );
+  if (error instanceof BaseError) {
+    logger.warn(
+      `${request.method} ${request.url} resulted in ${error.statusCode} with message ${error.message}`
+    );
 
-        await response.status(error.statusCode).send(error.message);
-    } else if (error instanceof Error) {
-        logger.error(
-            `${request.method} ${request.url} resulted in ${StatusCodes.INTERNAL_SERVER_ERROR} with message ${error.message}`
-        );
-        console.log(error.stack);
+    await response.status(error.statusCode).send(error.message);
+  } else if (error instanceof Error) {
+    logger.error(
+      `${request.method} ${request.url} resulted in ${StatusCodes.INTERNAL_SERVER_ERROR} with message ${error.message}`
+    );
+    console.log(error.stack);
 
-        await response
-            .status(StatusCodes.INTERNAL_SERVER_ERROR)
-            .send(error.message);
-    } else {
-        logger.error(
-            `${request.method} ${request.url} resulted in ${StatusCodes.INTERNAL_SERVER_ERROR} with message`
-        );
+    await response
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .send(error.message);
+  } else {
+    logger.error(
+      `${request.method} ${request.url} resulted in ${StatusCodes.INTERNAL_SERVER_ERROR} with message`
+    );
 
-        await response
-            .status(StatusCodes.INTERNAL_SERVER_ERROR)
-            .send("UNKNOWN");
-    }
-    next(error);
+    await response.status(StatusCodes.INTERNAL_SERVER_ERROR).send("UNKNOWN");
+  }
+  next(error);
 };
 
 export default errorMiddleware;
