ab23597f50a35c5c14d22518585e0a1ae644b482
yotam <yotam201410@gmail.com>
2024-08-17 22:22:27 +0300

    removed http only
    i hate myself after 2 days of debugging turns out httpOnly doesnt mean about https and http but that you can only read and use this cookie in the context of the http protocol and packets but not with DOM or js code

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -11,9 +11,9 @@ export const registerHandler = async (
   try {
     const userData: CreateUserDto = request.body;
 
-    const { cookie, id } = await register(userData);
-    response.cookie("token", cookie, { httpOnly: true });
-    response.status(StatusCodes.CREATED).json({ id });
+    const { cookie } = await register(userData);
+    response.cookie("token", cookie, { maxAge: 60 * 60 * 1000 });
+    response.status(StatusCodes.CREATED).send("successfully registered");
   } catch (err) {
     next(err);
   }
@@ -26,10 +26,10 @@ export const loginHandler = async (
 ) => {
   try {
     const userData: UserLoginDTO = request.body;
-    const { cookie, id } = await login(userData);
+    const { cookie } = await login(userData);
 
-    response.cookie("token", cookie, { httpOnly: true, maxAge:60*60*1000 });
-    response.status(StatusCodes.OK).json({ id });
+    response.cookie("token", cookie, { maxAge: 60 * 60 * 1000 });
+    response.status(StatusCodes.OK).send("successfully logged in");
   } catch (err) {
     next(err);
   }
