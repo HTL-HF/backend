7ff239898ac6bfc8e9ca3a296343944ef4816202
yotam <yotam201410@gmail.com>
2024-08-19 00:47:13 +0300

    created middleware to verify JWT token

---
+++ src/middlewares/verifyTokenMiddleware.ts
@@ -0,0 +1,31 @@
+import { Request, Response, NextFunction } from "express";
+import { verify } from "jsonwebtoken";
+import UnauthorizedError from "../errors/UnauthorizedError";
+import { verifyToken } from "../utils/jwt.utils";
+
+const verifyTokenMiddleware = async (
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  try {
+    if (request.cookies && request.cookies["token"]) {
+      const token = request.cookies["token"].token;
+
+      try {
+        verifyToken(token);
+      } catch (err) {
+        throw new UnauthorizedError("Invalid JWT token");
+      }
+    } else {
+      throw new UnauthorizedError("You must include an authentication token");
+    }
+
+    next();
+  } catch (err) {
+    
+    next(err);
+  }
+};
+
+export default verifyTokenMiddleware
