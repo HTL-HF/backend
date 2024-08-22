8b87ce720cbe20045cb95d76dac522c24cd29291
yotam201410 <yotam201410@gmail.com>
2024-08-22 10:18:20 +0300

    changed routes order to allow some routes with token and some without

--- src/routes/forms.routes.ts
+++ src/routes/forms.routes.ts
@@ -5,11 +5,24 @@ import {
   deleteFormHandler,
   getFormByIdHandler,
 } from "../controllers/forms.controller";
+import {
+  getResponsesHandler,
+  removeResponseHandler,
+  saveResponseHandler,
+} from "../controllers/responses.controller";
 
 const formsRouter = Router();
 
+formsRouter.post("/:formId/responses", saveResponseHandler);
+formsRouter.get("/:id", getFormByIdHandler);
+
 formsRouter.post("/", addFormHandler).use(verifyTokenMiddleware);
 formsRouter.delete("/:id", deleteFormHandler).use(verifyTokenMiddleware);
-formsRouter.get("/:id",getFormByIdHandler);
-
+formsRouter
+  .get("/:formId/responses", getResponsesHandler)
+  .use(verifyTokenMiddleware);
+formsRouter
+  .delete("/:formId/responses/:id", removeResponseHandler)
+  .use(verifyTokenMiddleware);
+  
 export default formsRouter;
