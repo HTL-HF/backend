f282df32caf194ef775ff684bde6564c5e2e3c60
yotam201410 <yotam201410@gmail.com>
2024-09-04 11:44:08 +0300

    moved response validation to utils

--- src/services/responses.service.ts
+++ src/services/responses.service.ts
@@ -13,6 +13,7 @@ import { UserDTO } from "../types/dtos/users.dto";
 import { QuestionModel } from "../types/interfaces/forms.interface";
 import { inArray } from "../utils/array.utils";
 import { getUserFromToken, verifyToken } from "../utils/jwt.utils";
+import { verifyResponseValidity } from "../utils/validation.utils";
 import { getFormById, isOwner } from "./forms.service";
 
 export const getFormResponsesById = async (
@@ -83,67 +84,5 @@ export const removeResponse = async (responseId: string, user:UserDTO) => {
   await deleteResponse(responseId);
 
   return response;
-};
-
-const verifyResponseValidity = async (
-  response: Omit<ResponseDTO, "id" | "formId">,
-  formId: string
-) => {
-  const questions = (await getFormById(formId)).questions;
-
-  for (const question of questions) {
-    const answer = response.answers.find(
-      (answer) => answer.questionId === question.id.toString()
-    );
-
-    if (!answer) {
-      if (question.required)
-        throw new NotAcceptableError(
-          `question ${question.id} is required but was not answered`
-        );
-    } else {
-      assertTypes(question, answer);
-      assertOptions(question, answer);
-    }
-  }
-};
-
-const assertTypes = (question: ResponseQuestion, answer: AnswerDTO) => {
-  if (
-    question.type !== typeof answer.answer &&
-    !(
-      Array.isArray(answer.answer) &&
-      answer.answer.every((answer) => typeof answer === typeof question.type)
-    )
-  ) {
-    throw new NotAcceptableError(
-      `question ${question.id} is of type ${
-        question.type
-      } and answer is of type ${
-        Array.isArray(answer.answer)
-          ? typeof answer.answer[0]
-          : typeof answer.answer
-      } `
-    );
-  }
-};
-
-const assertOptions = (question: ResponseQuestion, answer: AnswerDTO) => {
-  if (!question.options) return;
-
-  const { viewType } = question;
-  const { answer: ans } = answer;
-
-  if (viewType === "CHECKBOX") {
-    if (!Array.isArray(ans) || !ans.every((item) => question.options!.includes(item))) {
-      throw new NotAcceptableError(
-        `Your answer must be an array and part of the available options for question ${question.id}`
-      );
-    }
-  } else if (Array.isArray(ans) || !question.options.includes(ans)) {
-    throw new NotAcceptableError(
-      `Your answer must be a single value and part of the available options for question ${question.id}`
-    );
-  }
 };
 
